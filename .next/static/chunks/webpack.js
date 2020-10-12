3aa2680dbf2931c83f99155a86e2932946201919
jorge arguelles <jorgeariasarguelles@gmail.com>
2020-10-12 18:33:53 -0500

    Check

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7eca4e64ded33b5d3164";
+/******/ 	var hotCurrentHash = "21707ca0f09ac91582ca";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
