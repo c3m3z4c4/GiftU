8afd110110f6de30320d81c6ad8d4c83d42abaa8
jorge arguelles <jorgeariasarguelles@gmail.com>
2020-10-14 21:57:44 -0500

    prueba

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Header from "../components/header";
 import Main from "../components/main";
 import Footer from "../components/footer";
 const image = "images/footer.png";
-<<<<<<< HEAD
 
 const App = () => {
 	return (
@@ -21,22 +20,5 @@ const App = () => {
 			/>
 		</Fragment>
 	);
-=======
-const App = () => {
-  return (
-    <Fragment>
-      <GlobalStyle />
-      <Header />
-      <Main />
-      <Footer
-        texto="¿Ya tienes una cuenta?"
-        loginText="Inicia sesión"
-        footerImage={image}
-        altImage="Demo"
-      />
-    </Fragment>
-  );
->>>>>>> 37e1b8d388f3b294302c395ea5656c3a8bb66a53
-};
 
 export default App;
