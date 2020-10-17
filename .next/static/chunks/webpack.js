06e8fba30029c96e70301572f96d6d03f01d8592
jorge arguelles <jorgeariasarguelles@gmail.com>
2020-10-16 23:45:48 -0500

    [ADD] Search page finish! :)

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b103bde9bce742f1fbea";
+/******/ 	var hotCurrentHash = "f63c6326b4d873a05036";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
