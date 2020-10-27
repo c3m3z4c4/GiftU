64019d24158b88ebd8234ef038832f531adcfd5b
sue <sue@MacBook-Pro-de-sue.local>
2020-10-27 01:37:09 -0600

    add bundle

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9664fd3a74ed3f6b77ac";
+/******/ 	var hotCurrentHash = "894d0843b5e9b59ca3f4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
