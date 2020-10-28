df333743fed14064fa90c5de917764c7e032b4a8
sue <sue@MacBook-Pro-de-sue.local>
2020-10-28 03:03:54 -0600

    bundle

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "894d0843b5e9b59ca3f4";
+/******/ 	var hotCurrentHash = "c1562f4ca9fc0a174bac";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
