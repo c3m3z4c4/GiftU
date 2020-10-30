da08bbc9d9a43dd4926db56d83b7eb831928dc9f
sue <sue@MacBook-Pro-de-sue.local>
2020-10-30 02:09:22 -0600

    refactor form for search

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e593217aae07e349977f";
+/******/ 	var hotCurrentHash = "d26ed890560ba5bf5bfb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/SearchRight/index.js
+++ components/SearchRight/index.js
@@ -1,32 +1,42 @@
 import React, { Fragment } from "react";
 import Link from "next/link";
-
+// import { gql, useMutation } from '@apollo/client'
 import Menu from "../Menu";
 import { OptButton } from "../OptButton";
 import {
 	WrapperRight,
 	WrappperMenu,
 	WrapperForm,
-	WrapperGender,
-	TextGender,
-	WrapperImg,
-	WrapperOccacion,
-	TextOccacion,
-	SelectOccacion,
 	WrapperPerfiles,
 	TextPerfiles,
 	WrapperLabels,
 	WrapperLabel,
-	imgInt,
 	imgFB,
 	LabelRS,
 	InputRS,
 	WrapperButtom,
 	BaseButton,
 	Span,
+	Textform,
 } from "./styles";
 
+// const SEND_INFORMATION = gql`
+// 	mutation{
+// 		social_network() {
+// 			id_receiver
+// 			id_social_network
+// 			url_social_network
+// 		}
+// 	}
+// `;
+// const variables = { 
+// 	id_receiver: 4,
+// 	id_social_network: 3,
+// 	url_social_network: "URL"
+// };
+
 const SearchRight = () => {
+	// const [sendInrmation, { data }] = useMutation(SEND_INFORMATION);
 	return (
 		<Fragment>
 			<WrapperRight>
@@ -34,29 +44,18 @@ const SearchRight = () => {
 					<Menu />
 				</WrappperMenu>
 				<WrapperForm>
-					<WrapperGender>
-						<TextGender>
-							<Span primary>G</Span>énero:
-						</TextGender>
-						<WrapperImg>
-							<OptButton src="/images/she-opt.png" />
-							<OptButton src="/images/he-opt.png" />
-						</WrapperImg>
-					</WrapperGender>
-					<WrapperOccacion>
-						<TextOccacion>
-							O<Span>c</Span>asión:
-						</TextOccacion>
-						<SelectOccacion>
-							<option value="Bautizo">Bautizo</option>
-							<option value="Boda">Boda</option>
-							<option value="Navidad">Navidad</option>
-							<option value="Aniversario" selected>
-								Aniversario
-							</option>
-						</SelectOccacion>
-					</WrapperOccacion>
-					<WrapperPerfiles>
+					<Textform>
+						¿Cómo se <Span primary>llama </Span>esa persona <Span>especial</Span> ?
+					</Textform>
+					<InputRS />
+					<Textform>
+						prestanos su <Span>Facebook:</Span>
+					</Textform>
+					<WrapperLabel>
+						<LabelRS src={imgFB} />
+						<InputRS></InputRS>
+					</WrapperLabel>
+					{/* <WrapperPerfiles>
 						<TextPerfiles>
 							Pe<Span primary>r</Span>files:
 						</TextPerfiles>
@@ -66,14 +65,13 @@ const SearchRight = () => {
 								<InputRS></InputRS>
 							</WrapperLabel>
 							<WrapperLabel>
-								<LabelRS src={imgInt} />
-								<InputRS></InputRS>
+								<InputRS />
 							</WrapperLabel>
 						</WrapperLabels>
-					</WrapperPerfiles>
+					</WrapperPerfiles> */}
 					<WrapperButtom>
 						<Link href="/podium">
-							<BaseButton>
+							<BaseButton type="submit">
 								EN<Span primary>V</Span>IAR
 							</BaseButton>
                         </Link>
