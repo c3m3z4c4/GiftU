aca0b869e635706e8267260148706f38949da0a7
Cesar Meza <deorsoft@gmail.com>
2020-11-04 14:05:08 -0600

    Previous sync

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/access":["static\u002Fchunks\u002Fpages\u002Faccess.js"],"/search":["static\u002Fchunks\u002Fpages\u002Fsearch.js"],sortedPages:["\u002F_app","\u002F_error","\u002Faccess","\u002Fsearch"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/podium":["static\u002Fchunks\u002Fpages\u002Fpodium.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fpodium"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> 3e97eb1acb437d40e277b5902010a05470cc3de0
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/details":["static\u002Fchunks\u002Fpages\u002Fdetails.js"],"/podium":["static\u002Fchunks\u002Fpages\u002Fpodium.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fdetails","\u002Fpodium"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
