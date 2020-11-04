23029862eb15952663062eaa6c0f688827ea4388
Cesar Meza Carrillo <39112492+c3m3z4@users.noreply.github.com>
2020-11-04 11:35:42 -0600

    Update index.js
    Modigy Header location

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 
-import Header from "../components/header";
+import Header from "../components/Header";
 import Main from "../components/Main";
 import Footer from "../components/Footer";
 
