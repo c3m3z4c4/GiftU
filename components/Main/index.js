2686da588fb934083e35ae2462d93573efcd0a29
C3M3Z4 <cmeza@mezadigital.com>
2020-11-07 10:33:22 -0600

    [UPDATE] Modify main component

--- components/Main/index.js
+++ components/Main/index.js
@@ -15,45 +15,48 @@ import {
 import Auth from '../../lib/Auth';
 const auth = new Auth();
 
-const Main = () => {
- const handleLogin = () => {
-    auth.login()
-  }
 
-	return (
-		<Fragment>
-			<Wrapper>
-				<WrapperTitle>
-					<Title>
-					<Span>H</Span>OLA,<br />¿<Span primary>N</Span>O <br />
-						SABES <br />
-						Q<Span primary>U</Span>E <br />
-						REGALAR <Span>?</Span>
-					</Title>
-				</WrapperTitle>
-				<WrapperGifts>
-					<Wrappergift>
-						<Link href="/search">
-							<GiftTitle>
-								REGALO <br />
-								RECOM<Span>É</Span>NDADO
-							</GiftTitle>
-						</Link>
-						<ColorLine primary />
-					</Wrappergift>
-					<Wrappergift>
-						<LinkButton onClick={()=>handleLogin()} > 
-							<GiftTitle>
-								REGALO <br />
-								ID<Span primary>E</Span>AL
-							</GiftTitle>
-						</LinkButton>
-						<ColorLine />
-					</Wrappergift>
-				</WrapperGifts>
-			</Wrapper>
-		</Fragment>
-	);
-};
+export default class Main extends React.Component {
 
-export default Main;
+	handleLogin = () => {
+		auth.login()
+	}
+
+	render() {
+		return (
+			<Fragment>
+				<Wrapper>
+					<WrapperTitle>
+						<Title>
+							<Span>H</Span>OLA,<br />¿<Span primary>N</Span>O <br />
+							SABES <br />
+							Q<Span primary>U</Span>E <br />
+							REGALAR <Span>?</Span>
+						</Title>
+					</WrapperTitle>
+					<WrapperGifts>
+						<Wrappergift>
+							<Link href="/search">
+								<GiftTitle>
+									REGALO <br />
+									RECOM<Span>É</Span>NDADO
+								</GiftTitle>
+							</Link>
+							<ColorLine primary />
+						</Wrappergift>
+						<Wrappergift>
+							<LinkButton onClick={() => this.handleLogin()} >
+								<GiftTitle>
+									REGALO <br />
+									ID<Span primary>E</Span>AL
+								</GiftTitle>
+							</LinkButton>
+							<ColorLine />
+						</Wrappergift>
+					</WrapperGifts>
+				</Wrapper>
+			</Fragment>
+		);
+	};
+}
+
