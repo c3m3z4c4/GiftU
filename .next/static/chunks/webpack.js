b9b6578a7f38baff9c90dee468ecf3475d81621d
sue <sue@MacBook-Pro-de-sue.local>
2020-11-07 22:24:26 -0600

    add manifest

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7919e127fb28595931bd";
+/******/ 	var hotCurrentHash = "6f61a0a9b10a512126f3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
