5053c703f8e3f0fe0a9c5bee36d9aa9cf0bc7f7c
sue <sue@MacBook-Pro-de-sue.local>
2020-11-11 02:30:00 -0600

    addc load and error in form

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1d092461fcc08a7984a6";
+/******/ 	var hotCurrentHash = "035ca8aa7c85af6cf1ef";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
