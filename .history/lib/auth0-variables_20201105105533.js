4d98f1ff3971730ef79172a3e55f770a774bb110
C3M3Z4 <cmeza@mezadigital.com>
2020-11-11 22:38:24 -0600

    Cleaning next folder

--- .history/lib/auth0-variables_20201105105533.js
+++
@@ -1,5 +0,0 @@
-export const AUTH_CONFIG = {
-	domain: "dev-hn1aj6kz.us.auth0.com",
-	clientId: "gPlNfuoWiPQ7fiGMCi4V2HCWo6BHGRXD",
-	callbackUrl: "https://giftu-nu.vercel.app/auth/callback",
-};
--- .history/lib/auth0-variables_20201106105308.js
+++
@@ -1,5 +0,0 @@
-export const AUTH_CONFIG = {
-    domain: "dev-hn1aj6kz.us.auth0.com",
-    clientId: "gPlNfuoWiPQ7fiGMCi4V2HCWo6BHGRXD",
-    callbackUrl: "http://localhost:3000/auth/callback",
-};
